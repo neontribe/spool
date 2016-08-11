091b3d5cef28d270bd23fd89c5e9f394aa62b0c0
Rupert Redington <rupert@neontribe.co.uk>
2016-08-11 13:21:34 +0100

    Backend Arrives

--- src/server/index.js
+++ src/server/index.js
@@ -1,23 +1,18 @@
-var http = require('http');
-var app = http.createServer(function(request, response) {
-    response.writeHead(200, {
-        'Content-Type': 'text/html',
-        'X-Powered-By': 'hope'
-    });
-    response.end(`
-        <html lang="en">
-            <head/>
-            <body>
-                <h1>SPOOL BEGINS!</h1>
-                <p>Sprint Zero. No features to see yet.</p>
-            </body>
-        </html>
-    `);
-});
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema.js');
+
+var app = express();
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    pretty: true,
+    graphiql: true
+}));
 
 module.exports = app;
 
-/* istanbul ignore if */
+/* instanbul ignore if */
 if (!module.parent) {
     app.listen(process.env.PORT || 3001);
 }
