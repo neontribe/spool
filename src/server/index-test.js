ca3d24656d8209f8b360f16300a7b96bb0d8eb89
Rupert Redington <rupert@neontribe.co.uk>
2016-08-11 13:53:15 +0100

    Fix coverage

--- src/server/index-test.js
+++ src/server/index-test.js
@@ -1,10 +1,10 @@
 var app = require('./index.js');
 var request = require('supertest');
 
-describe('Placeholder app', function() {
-    it('should serve a 200 at its root', function(done){
+describe('graphql endpoint', function() {
+    it('should serve a 400 at its root', function(done){
         request(app)
-            .get('/')
-            .expect(200, done);
+            .get('/graphql')
+            .expect(400, done);
     });
 });
