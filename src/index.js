cdfd1cf61703c46e2ef34a439b32eeae8de8e78d
Rupert Redington <rupert@neontribe.co.uk>
2016-08-16 00:22:21 +0100

    Add Auth0

--- src/index.js
+++ src/index.js
@@ -1,16 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { Router, Route, IndexRedirect, hashHistory } from 'react-router';
+import AuthService from './auth/AuthService';
 import App from './App';
 import Home from './components/Home';
+import Login from './components/Login';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 
+const auth = new AuthService(process.env.REACT_APP_AUTH0_CLIENT_ID, process.env.REACT_APP_AUTH0_DOMAIN);
+
+// onEnter callback to validate authentication in private routes
+const requireAuth = (nextState, replace) => {
+  if (!auth.loggedIn()) {
+    replace({ pathname: '/login' });
+  }
+};
+// OnEnter for callback url to parse access_token
+const parseAuthHash = (nextState, replace) => {
+  auth.parseHash(nextState.location.hash);
+  replace('/home');
+}
+
 ReactDOM.render(
-  <Router history={browserHistory}>
-    <Route path="/" component={App}>
-        <IndexRoute component={Home} />
+  // TODO: Shift to browserHistory only blocked by auth0 access_token handling
+  // see: https://auth0.com/forum/t/having-trouble-with-login-following-the-react-guide/3084
+  <Router history={hashHistory}>
+    <Route path="/" component={App} auth={auth}>
+        <IndexRedirect to="/home" />
+        <Route path="home" component={Home} onEnter={requireAuth} />
+        <Route path="login" component={Login} />
+        <Route path="access_token=:token" onEnter={parseAuthHash} />
     </Route>
   </Router>,
   document.getElementById('root')
