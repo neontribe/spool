ca58eb74ec024dea2aaf71335fb91a4a9ed48b12
Rupert Redington <rupert@neontribe.co.uk>
2016-08-16 17:18:30 +0100

    MediaTypeChooser and media form switching

--- src/components/stories/MediaTypeChooser.js
+++ src/components/stories/MediaTypeChooser.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { storiesOf, action } from '@kadira/storybook';
-import MediaForm from '../MediaForm';
+import MediaTypeChooser from '../MediaTypeChooser';
 import { Grid, Row, Col } from 'react-bootstrap';
 
-storiesOf('MediaForm', module)
+storiesOf('MediaTypeChooser', module)
   .add('Initial View', () => (
     <Grid>
         <Row>
             <Col>
-                <MediaForm save={action('Save Value')}/>
+                <MediaTypeChooser save={action('Save Value')}/>
             </Col>
         </Row>
     </Grid>
