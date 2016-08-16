57e43abec57c196b8ee1ce41359381a80b3a7fdf
Rupert Redington <rupert@neontribe.co.uk>
2016-08-16 14:23:34 +0100

    ProfileLink Component

--- src/components/stories/fixtures.js
+++ src/components/stories/fixtures.js
@@ -41,3 +41,28 @@ export const entries =  [
         topic: ['Sport']
     },
 ];
+
+export const googleProfile = {
+	"email": "fernando@example.com",
+	"email_verified": true,
+	"name": "Fernando Hoss",
+	"given_name": "Fernando",
+	"family_name": "Hoss",
+	"picture": "https://lh3.googleusercontent.com/-HG2Xt5yec7U/AAAAAAAAAAI/AAAAAAAAAmc/PUccB8Tp_4k/photo.jpg",
+	"gender": "male",
+	"locale": "en-GB",
+	"clientID": "seekrit",
+	"updated_at": "2016-08-16T12:41:43.462Z",
+	"user_id": "google-oauth2|xxxx",
+	"nickname": "fernando.hoss",
+	"identities": [
+		{
+			"provider": "google-oauth2",
+			"user_id": "xxxx",
+			"connection": "google-oauth2",
+			"isSocial": true
+		}
+	],
+	"created_at": "2016-08-15T23:01:02.918Z",
+	"global_client_id": "xxxx"
+};
