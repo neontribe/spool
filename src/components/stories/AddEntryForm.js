117924f00400eecfdc59a0307ae19533b7a63e49
Anthony Stansbridge <github@anthonystansbridge.co.uk>
2016-08-17 17:45:44 +0100

    Added an 'AddEntryMutation'

--- src/components/stories/AddEntryForm.js
+++ src/components/stories/AddEntryForm.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { storiesOf } from '@kadira/storybook';
-import AddEntryForm from '../AddEntryForm';
+import { AddEntryForm } from '../AddEntryForm';
 import { Grid, Row, Col } from 'react-bootstrap';
 
 storiesOf('AddEntryForm', module)
