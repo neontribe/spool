b8c78db644dde0d0975cb10cf22f54e2b1203873
Anthony Stansbridge <github@anthonystansbridge.co.uk>
2016-08-22 14:18:43 +0100

    Removed seed-database, added setup-database

---
+++ setup.sql
@@ -0,0 +1,3 @@
+INSERT INTO topic_type (type) VALUES ('transport'), ('shopping'), ('health'), ('entertainment'), ('sport'), ('public_transport');
+INSERT INTO media_type (type) VALUES ('text'), ('video'), ('image');
+INSERT INTO sentiment_type (type) VALUES ('happy'), ('sad');
