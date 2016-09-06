5d43e9dd1d2ddea5990a52fea46ef345b1a1de04
Rupert Redington <rupert@neontribe.co.uk>
2016-09-06 16:40:49 +0100

    Fix storybook

--- src/components/stories/index.js
+++ src/components/stories/index.js
@@ -1,10 +1,8 @@
 require('./Entry.js');
 require('./EntryViewer.js');
 require('./Timeline.js');
-require('./AddEntryForm.js');
 require('./Home.js');
 require('./ProfileLink');
-require('./EntryForm');
 require('./TextForm');
 require('./VideoForm');
 require('./VideoRecorder');
