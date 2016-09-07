3eaac86fc46385d8b01477d86c242f6adbbbc3fc
Anthony Stansbridge <github@anthonystansbridge.co.uk>
2016-09-07 13:49:34 +0100

    Removed unused imports from IconCard component.

--- src/components/IconCard.js
+++ src/components/IconCard.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { FormGroup, ControlLabel, Button, ButtonToolbar, Glyphicon, Grid, Row, Col } from 'react-bootstrap';
 
 export class IconCard extends Component {
     static propTypes = {
