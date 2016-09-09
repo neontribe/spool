9ba6fd0e10c1d8262b42a4524df8f04f99340417
Rupert Redington <rupert@neontribe.co.uk>
2016-09-09 15:57:18 +0100

    Fix storybook

--- src/components/stories/Dashboard.js
+++ src/components/stories/Dashboard.js
@@ -2,7 +2,16 @@ import React from 'react';
 import { storiesOf } from '@kadira/storybook';
 import { Dashboard } from '../Dashboard';
 
+const viewer = {
+    role: {
+        creatorActivityCount: {
+            active: 10,
+            stale: 3
+        }
+    }
+};
+
 storiesOf('Dashboard', module)
   .add('Initial View', () => (
-    <Dashboard />
+    <Dashboard viewer={viewer}/>
   ));
