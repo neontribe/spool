8ddb31956f008caa24307c675d54182001e3f3ca
Anthony Stansbridge <github@anthonystansbridge.co.uk>
2016-12-13 13:22:23 +0000

    Moved add entry code away from the router

--- src/components/MediaForm.js
+++ src/components/MediaForm.js
@@ -3,62 +3,82 @@ import { withRouter } from 'react-router';
 import _ from 'lodash';
 
 import IconChooser from './IconChooser';
+import TextForm from './TextForm';
+import VideoForm from './VideoForm';
+import ImageForm from './ImageForm';
 
 import styles from './css/MediaForm.module.css';
 
+
 const choices = [
-  {
-    type: 'video',
-    name: 'Video'
-  },
-  {
-    type: 'photo',
-    name: 'Picture'
-  },
-  {
-    type: 'typing',
-    name: 'Write'
-  }
+    {
+        type: 'video',
+        name: 'Video'
+    },
+    {
+        type: 'photo',
+        name: 'Picture'
+    },
+    {
+        type: 'typing',
+        name: 'Write'
+    }
 ];
 
 class MediaForm extends Component {
-  constructor (props) {
-    super(props);
+    static SELECT = 'MEDIAFORM/SELECT';
+    static VIDEO = 'MEDIAFORM/VIDEO';
+    static PHOTO = 'MEDIAFORM/PHOTO';
+    static TEXT = 'MEDIAFORM/TEXT';
 
-    this.handleNext = this.handleNext.bind(this);
-  }
+    constructor (props) {
+        super(props);
+        this.handleSave = _.partial(props.save, props.saveKey);
 
-  handleNext (values) {
-    this.props.router.push('/add/message/' + values[0]);
-  }
-
-  renderChooser () {
-    return (
-      <IconChooser
-        label='How would you like to create the entry?'
-        choices={choices}
-        maxSelections={1}
-        onChange={this.handleNext}
-      />
-    );
-  }
-
-  render () {
-    let children = null;
-
-    if (this.props.children) {
-      children = React.cloneElement(this.props.children, {
-        save: _.partial(this.props.save, this.props.saveKey)
-      });
+        const { SELECT, VIDEO, PHOTO, TEXT } = MediaForm;
+        const choiceMap = {
+            video: VIDEO,
+            photo: PHOTO,
+            typing: TEXT,
+        }
+        this.state = {
+            form: SELECT
+        }
+        this.chooserTransition = (values) => this.setState({form: choiceMap[values[0]]});
     }
 
-    return (
-      <div className={styles.wrapper}>
-        {children && children}
-        {!children && this.renderChooser()}
-      </div>
-    );
-  }
+    renderChooser () {
+        return (
+            <IconChooser
+            label='How would you like to create the entry?'
+            choices={choices}
+            maxSelections={1}
+            onChange={this.chooserTransition}
+            />
+        );
+    }
+
+    renderForm () {
+        const { SELECT, VIDEO, PHOTO, TEXT } = MediaForm;
+        switch(this.state.form) {
+            case SELECT:
+                return this.renderChooser();
+            case VIDEO:
+                return <VideoForm save={this.handleSave}/>;
+            case PHOTO:
+                return <ImageForm save={this.handleSave}/>;
+            case TEXT:
+                return <TextForm save={this.handleSave}/>;
+        }
+    }
+
+    render () {
+        return (
+            <div className={styles.wrapper}>
+                {this.renderForm()}
+            </div>
+        );
+    }
 }
 
 MediaForm.propTypes = {
