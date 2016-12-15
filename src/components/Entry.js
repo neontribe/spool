ab22edd92dc31e751af05ecd09545865d8db811c
Matt Layton <mattlayt@gmail.com>
2016-12-15 11:46:40 +0000

    Toggle play/pause video on EntryViewer screen. Hide overlay whilst video is playing.

--- src/components/Entry.js
+++ src/components/Entry.js
@@ -25,10 +25,30 @@ export class Entry extends Component {
     constructor (props) {
         super(props);
 
-        this.state = {};
+        this.state = {
+            showOverlay: true
+        };
 
         this.showViewer = this.showViewer.bind(this);
         this.hideViewer = this.hideViewer.bind(this);
+        this.toggleVideoPlay = this.toggleVideoPlay.bind(this);
+    }
+
+    componentDidMount () {
+        if (this.refs && this.refs.video) {
+            this.refs.video.onplaying = () => {
+                this.setState({
+                    showOverlay: false
+                });
+            };
+
+            // this.refs.video.onpause
+            this.refs.video.onended = () => {
+                this.setState({
+                    showOverlay: true
+                });
+            };
+        }
     }
 
     showViewer (e) {
@@ -47,15 +67,82 @@ export class Entry extends Component {
         });
     }
 
+    toggleVideoPlay () {
+        var video = this.refs.video;
+
+        if (video.paused) {
+            video.play();
+        } else {
+            video.pause();
+        }
+    }
+
+    renderEntry (entry, randomisedStyle, isTextEntry, lightIcon) {
+        return (
+            <div>
+                {isTextEntry && (
+                    <blockquote className={styles.text}>
+                        {entry.media.text.substring(0, 30)}
+                        {(entry.media.text.length > 30) && '...'}
+                    </blockquote>
+                )}
+
+                {entry.media.video && (
+                    <div className={styles.videoOverlay}>
+                        {this.state.showOverlay && (
+                            <div className={styles.videoOverlayPlay}></div>
+                        )}
+
+                        {!this.props.thumbnailMode && (
+                            <video
+                                className={styles.video}
+                                ref='video'
+                                src={entry.media.video}
+                                controls={true}
+                                autoPlay={true}
+                                onClick={!this.props.thumbnailMode && this.toggleVideoPlay}
+                            />
+                        )}
+                    </div>
+                )}
+
+                {this.props.showSentimentOverlay && (
+                    <Icon
+                        icon={entry.sentiment.type}
+                        light={lightIcon}
+                        size={4}
+                        className={styles.sentiment}
+                    />
+                )}
+
+                {this.props.thumbnailMode && (
+                    <div className={styles.date}>
+                        Created {moment(entry.created).format('Do MMMM')}
+                    </div>
+                )}
+
+                {this.props.showTopicOverlay && (
+                    <ul className={styles.topics}>
+                        {entry.topics.map((topic, i) => (
+                            <li key={i}>
+                                <Icon
+                                    icon={topic.type}
+                                    light={lightIcon}
+                                />
+                            </li>
+                        ))}
+                    </ul>
+                )}
+            </div>
+        );
+    }
+
     render () {
         var entry = this.props.entry;
-        var sentiment = entry.sentiment.type;
-        var text = entry.media.text;
-        var image = entry.media.image;
-        var video = entry.media.video;
+        var isTextEntry = entry.media.text && !entry.media.image && !entry.media.video;
         var styleVariant = styles.entry;
-        var randomisedStyle, backgroundImage;
-        var isTextEntry = text && !image && !video;
+        var randomisedStyle;
+        var backgroundImage;
         var lightIcon = true;
 
         if (isTextEntry) {
@@ -65,70 +152,33 @@ export class Entry extends Component {
             if (randomisedStyle.dark) {
                 lightIcon = false;
             }
-        } else if (image) {
+        } else if (entry.media.imageThumbnail) {
             backgroundImage = entry.media.imageThumbnail;
-        } else if (video && this.props.thumbnailMode) {
+        } else if (entry.media.video && this.props.thumbnailMode) {
             backgroundImage = entry.media.videoThumbnail;
         }
 
+        var props = {
+            className: styleVariant
+        };
+
+        if (backgroundImage) {
+            props.style = {
+                backgroundImage: `url(${backgroundImage})`
+            }
+        }
+
+        if (this.props.thumbnailMode) {
+            return (
+                <Link
+                    {...props}
+                    to={`/entry/${entry.id}`}
+                >{this.renderEntry(entry, randomisedStyle, isTextEntry, lightIcon)}</Link>
+            );            
+        }
+
         return (
-            <Link
-                to={`/entry/${entry.id}`}
-                className={styleVariant}
-                style={backgroundImage && { backgroundImage: `url(${backgroundImage})` }}
-            >
-                <div>
-                    {isTextEntry && (
-                        <blockquote className={styles.text}>
-                            {text.substring(0, 30)}
-                            {(text.length > 30) && '...'}
-                        </blockquote>
-                    )}
-
-                    {video && (
-                        <div className={styles.videoOverlay}>
-                            <div className={styles.videoOverlayPlay}></div>
-
-                            {!this.props.thumbnailMode && (
-                                <video
-                                    className={styles.video}
-                                    src={video}
-                                    controls={true}
-                                    autoPlay={true}
-                                />
-                            )}
-                        </div>
-                    )}
-
-                    {this.props.showSentimentOverlay && (
-                        <Icon
-                            icon={entry.sentiment.type}
-                            light={lightIcon}
-                            size={4}
-                            className={styles.sentiment}
-                        />
-                    )}
-
-                    {this.props.thumbnailMode && (
-                        <div className={styles.date}>
-                            Created {moment(entry.created).format('Do MMMM')}
-                        </div>
-                    )}
-
-                    {this.props.showTopicOverlay && (
-                        <ul className={styles.topics}>
-                            {entry.topics.map((topic, i) => (
-                                <li key={i}>
-                                    <Icon
-                                        icon={topic.type}
-                                        light={lightIcon}
-                                    />
-                                </li>
-                            ))}
-                        </ul>
-                    )}
-                </div>
-            </Link>
+            <div {...props}>{this.renderEntry(entry, randomisedStyle, isTextEntry, lightIcon)}</div>
         );
     }
 }
