fa1666ff37edd51dd54569e9523b7460cf76b530
Anthony Stansbridge <github@anthonystansbridge.co.uk>
2016-12-22 15:41:53 +0000

    Added the ability to supply menu content to the hamburger slideout

---
+++ src/components/IconFilter.js
@@ -0,0 +1,154 @@
+import React, { Component } from 'react';
+import Icon from './Icon';
+import { Link } from 'react-router';
+
+import styles from './css/IconFilter.module.css';
+
+class Filter extends Component {
+    constructor(props) {
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+    }
+    handleClick() {
+        this.props.onChange(!this.props.active);
+    }
+    render () {
+        return (
+            <li className={styles.filter} onClick={this.handleClick}>
+                <a role='button' className={this.props.active ? styles.filterControlOn : styles.filterControl}>
+                    <Icon icon={this.props.icon} light={true} />
+                    <span className={styles.filterText}>{this.props.children}</span>
+                </a>
+            </li>
+        );
+    }
+}
+
+class LinkFilter extends Component {
+    render () {
+        return (
+          <li className={styles.filter}>
+              <Link to={this.props.linkTo} className={styles.filterControl}>
+                  {this.props.children}
+              </Link>
+          </li>
+        );
+    }
+}
+
+class FilterList extends Component {
+    render () {
+        return (
+            <ul className={styles.filterList}>
+                {this.props.children}
+            </ul>
+        );
+    }
+}
+
+export default class IconFilter extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            happy: false,
+            sad: false,
+            work: false,
+            learning: false,
+            home: false,
+            food: false,
+            relationships: false,
+            activities: false,
+            travel: false,
+            health: false,
+            video: false,
+            photo: false,
+            typing: false,
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handlers = {};
+        Object.keys(this.state).forEach((key) => this.handlers[key] = _.partial(this.handleChange, key));
+    }
+    handleChange(key, value) {
+        const state = {
+            ...this.state,
+            [key]: value
+        };
+        this.setState(state);
+        this.props.onChange(state);
+    }
+    render() {
+        const { happy, sad, work, learning, home, food, relationships, activities, travel, health, video, photo, typing } = this.state;
+        const handlers = this.handlers;
+        return (<div>
+          <div className={styles.filterBlock}>
+              <h2 className={styles.filterHeader}>View Mode</h2>
+              <FilterList>
+                  <LinkFilter linkTo='/app/home'>Gallery</LinkFilter>
+                  <LinkFilter linkTo='/app/timeline'>Timeline</LinkFilter>
+              </FilterList>
+          </div>
+
+          <div className={styles.filterBlock}>
+            <h2 className={styles.filterHeader}>Filters</h2>
+            <FilterList>
+                <Filter
+                    icon='happy'
+                    onChange={handlers['happy']}
+                    active={happy}>Happy</Filter>
+                <Filter
+                    icon='sad'
+                    onChange={handlers['sad']}
+                    active={sad}>Sad</Filter>
+            </FilterList>
+            <FilterList>
+                <Filter
+                    icon='work'
+                    onChange={handlers['work']}
+                    active={work}>Work</Filter>
+                <Filter
+                    icon='learning'
+                    onChange={handlers['learning']}
+                    active={learning}>Learning</Filter>
+                <Filter
+                    icon='home'
+                    onChange={handlers['home']}
+                    active={home}>Home</Filter>
+                <Filter
+                    icon='food'
+                    onChange={handlers['food']}
+                    active={food}>Food</Filter>
+                <Filter
+                    icon='relationships'
+                    onChange={handlers['relationships']}
+                    active={relationships}>People &amp; Relationships</Filter>
+                <Filter
+                    icon='activities'
+                    onChange={handlers['activities']}
+                    active={activities}>Activities</Filter>
+                <Filter
+                    icon='travel'
+                    onChange={handlers['travel']}
+                    active={travel}>Travel</Filter>
+                <Filter
+                    icon='health'
+                    onChange={handlers['health']}
+                    active={health}>Health</Filter>
+            </FilterList>
+            <FilterList>
+                <Filter
+                    icon='video'
+                    onChange={handlers['video']}
+                    active={video}>Videos</Filter>
+                <Filter
+                    icon='photo'
+                    onChange={handlers['photo']}
+                    active={photo}>Photos</Filter>
+                <Filter
+                    icon='typing'
+                    onChange={handlers['typing']}
+                    active={typing}>Written</Filter>
+            </FilterList>
+          </div>
+        </div>)
+    }
+}
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,7 +4,6 @@ import _ from 'lodash';
 
 import ProfileLink from './ProfileLink';
 import Hamburger from './Hamburger';
-import Icon from './Icon';
 
 import styles from './css/App.module.css';
 
@@ -88,113 +87,7 @@ class Header extends Component {
                 </ul>
 
                 {/* Todo: Inject a component from the router, e.g. filter controls */}
-                <div>
-                  <div className={styles.filterBlock}>
-                    <h2 className={styles.filterHeader}>View Mode</h2>
-                    <ul className={styles.filterList}>
-                      <li className={styles.filter}>
-                          <Link to='/app/home' className={styles.filterControlOn}>
-                          Gallery
-                        </Link>
-                      </li>
-                      <li className={styles.filter}>
-                          <Link to='/app/timeline' className={styles.filterControl}>
-                          Timeline
-                        </Link>
-                      </li>
-                    </ul>
-                  </div>
-
-                  <div className={styles.filterBlock}>
-                    <h2 className={styles.filterHeader}>Filters</h2>
-
-                    <ul className={styles.filterList}>
-                      <li className={styles.filter}>
-                        <a role='button' className={styles.filterControl}>
-                          <Icon icon='happy' light={true} />
-                          <span className={styles.filterText}>Happy</span>
-                        </a>
-                      </li>
-                      <li className={styles.filter}>
-                        <a role='button' className={styles.filterControl}>
-                          <Icon icon='sad' light={true} />
-                          <span className={styles.filterText}>Sad</span>
-                        </a>
-                      </li>
-                    </ul>
-
-                    <ul className={styles.filterList}>
-                      <li className={styles.filterNewRow}>
-                        <a role='button' className={styles.filterControlOn}>
-                          <Icon icon='work' light={true} />
-                          <span className={styles.filterText}>Work</span>
-                        </a>
-                      </li>
-                      <li className={styles.filter}>
-                        <a role='button' className={styles.filterControlOn}>
-                          <Icon icon='learning' light={true} />
-                          <span className={styles.filterText}>Learning</span>
-                        </a>
-                      </li>
-                      <li className={styles.filter}>
-                        <a role='button' className={styles.filterControlOn}>
-                          <Icon icon='home' light={true} />
-                          <span className={styles.filterText}>Home</span>
-                        </a>
-                      </li>
-                      <li className={styles.filter}>
-                        <a role='button' className={styles.filterControl}>
-                          <Icon icon='food' light={true} />
-                          <span className={styles.filterText}>Food</span>
-                        </a>
-                      </li>
-                      <li className={styles.filter}>
-                        <a role='button' className={styles.filterControl}>
-                          <Icon icon='relationships' light={true} />
-                          <span className={styles.filterText}>People & Relationships</span>
-                        </a>
-                      </li>
-                      <li className={styles.filter}>
-                        <a role='button' className={styles.filterControlOn}>
-                          <Icon icon='activities' light={true} />
-                          <span className={styles.filterText}>Activities</span>
-                        </a>
-                      </li>
-                      <li className={styles.filter}>
-                        <a role='button' className={styles.filterControlOn}>
-                          <Icon icon='travel' light={true} />
-                          <span className={styles.filterText}>Travel</span>
-                        </a>
-                      </li>
-                      <li className={styles.filter}>
-                        <a role='button' className={styles.filterControl}>
-                          <Icon icon='health' light={true} />
-                          <span className={styles.filterText}>Health</span>
-                        </a>
-                      </li>
-                    </ul>
-                    <ul className={styles.filterList}>
-                      <li className={styles.filterMedia}>
-                        <a role='button' className={styles.filterControl}>
-                          <Icon icon='video' light={true} />
-                          <span className={styles.filterText}>Videos</span>
-                        </a>
-                      </li>
-                      <li className={styles.filterMedia}>
-                        <a role='button' className={styles.filterControl}>
-                          <Icon icon='photo' light={true} />
-                          <span className={styles.filterText}>Photos</span>
-                        </a>
-                      </li>
-                      <li className={styles.filterMedia}>
-                        <a role='button' className={styles.filterControl}>
-                          <Icon icon='typing' light={true} />
-                          <span className={styles.filterText}>Written</span>
-                        </a>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
+                {this.props.menuContent}
               </Hamburger>
             )}
           </div>
