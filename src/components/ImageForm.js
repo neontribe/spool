9cd6648c1cb9e3e46a6a313e1a3bfab9ec89b41a
Rupert Redington <rupert@neontribe.co.uk>
2017-02-07 12:55:52 +0000

    Resize thumbnails

--- src/components/ImageForm.js
+++ src/components/ImageForm.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import _ from 'lodash';
 
 import uploadToS3 from '../s3';
+import resizer from '../lib/resizer.js';
 import Camera from './Camera';
 import ImageUploader from './ImageUploader';
 import PageOverlay from './PageOverlay';
@@ -46,34 +47,36 @@ class ImageForm extends Component {
         });
 
         this.props.onSaveStart();
-
-        var savers = _.toPairs(_.pick(data, 'image', 'imageThumbnail', 'video', 'videoThumbnail')).map((item) => {
-            return uploadToS3(item[1])
-                .then((s3Info) => {
-                    return {
-                        [item[0]]: s3Info
-                    };
-                });
-        });
-
-        if (data.text) {
-            savers.push({
-                text: data.text
+        resizer(data.imageThumbnail, 300, 300, (thumb) => {
+            data.imageThumbnail = thumb;
+            var savers = _.toPairs(_.pick(data, 'image', 'imageThumbnail')).map((item) => {
+                return uploadToS3(item[1])
+                    .then((s3Info) => {
+                        return {
+                            [item[0]]: s3Info
+                        };
+                    });
             });
-        }
 
-        Promise.all(savers)
-            .then((results) => {
-                var info = Object.assign.apply(Object, [{}].concat(results));
-
-                this.setState({
-                    uploading: false,
-                    uploaded: true
+            if (data.text) {
+                savers.push({
+                    text: data.text
                 });
+            }
 
-                this.props.onSaveEnd(info);
-            })
-            .catch((e) => console.log('Error during file save: ', e));
+            Promise.all(savers)
+                .then((results) => {
+                    var info = Object.assign.apply(Object, [{}].concat(results));
+
+                    this.setState({
+                        uploading: false,
+                        uploaded: true
+                    });
+
+                    this.props.onSaveEnd(info);
+                })
+                .catch((e) => console.log('Error during file save: ', e));
+        });
     }
 
     requestUploadMode () {
