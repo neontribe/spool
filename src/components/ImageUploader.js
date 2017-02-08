4d3c6cbd7c989a94c69084bce130430e5f42c973
Matt Layton <mattlayt@gmail.com>
2017-02-08 22:11:31 +0000

    Style updates

--- src/components/ImageUploader.js
+++ src/components/ImageUploader.js
@@ -117,7 +117,7 @@ class ImageUploader extends Component {
 
                         {(this.state.imageThumbnail) && (
                             <div>
-                                <img alt="Your last take" src={this.state.imageThumbnail} />
+                                <img alt="Your last take" src={this.state.imageThumbnail} className={styles.imageThumbnail} />
 
                                 {this.state.text && (
                                     <div className={styles.text}>{this.state.text}</div>
